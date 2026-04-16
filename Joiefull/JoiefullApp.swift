3a8d62133d952a184b86b890cdfa54fea626b51c
Jaouadbs <wk.boussalah@gmail.com>
2026-04-16 16:48:26 +0200

    Initial commit

---
+++ Joiefull/JoiefullApp.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullApp.swift
+//  Joiefull
+//
+//  Created by Jaouad on 16/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct JoiefullApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ JoiefullTests/JoiefullTests.swift
@@ -0,0 +1,17 @@
+//
+//  JoiefullTests.swift
+//  JoiefullTests
+//
+//  Created by Jaouad on 16/04/2026.
+//
+
+import Testing
+@testable import Joiefull
+
+struct JoiefullTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
